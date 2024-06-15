dce2a38491c665382d3de8fafa9b9337ac551590
Jesse Pinheiro <jessebpinheiro@hotmail.com>
2024-06-15 10:28:32 -0300

    feat: add server host

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.SERVER_PORT || 3000;
+  const host = process.env.SERVER_HOST || '0.0.0.0';
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVER_PORT);
+  await app.listen(port, host);
 }
 bootstrap();
